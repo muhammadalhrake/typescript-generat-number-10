893f145cb90145b470a5d3d5742b69c81b26a9f4
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-05-20 14:04:01 +0300

    1 file modified, 1 added

---
+++ handel.ts
@@ -0,0 +1,49 @@
+// imports
+import random from 'random';
+
+//helper function
+function between(min: number, max: number) {
+  return random.int(min, max);
+}
+function handlePos(arrayOfNum, digitsNum, getpos, setpos) {
+  let myNumber = arrayOfNum[getpos];
+  if (myNumber > 2 && myNumber < 5) {
+    arrayOfNum[getpos] -= 1;
+    let rand = between(0, setpos);
+    arrayOfNum[digitsNum - 1 - rand] += 1;
+    console.log("I'm in hnndelpos 1");
+  } else if (myNumber == 5 || myNumber == 6) {
+    arrayOfNum[getpos] -= 2;
+    let rand1 = between(0, setpos);
+    let rand2 = between(0, setpos);
+    arrayOfNum[digitsNum - 1 - rand1] += 1;
+    arrayOfNum[digitsNum - 1 - rand2] += 1;
+    console.log("I'm in hnndelpos 2");
+  } else if (myNumber > 6 && myNumber <= 9) {
+    arrayOfNum[getpos] -= 3;
+    let rand1 = between(0, setpos);
+    let rand2 = between(0, setpos);
+    let rand3 = between(0, setpos);
+    arrayOfNum[digitsNum - 1 - rand1] += 1;
+    arrayOfNum[digitsNum - 1 - rand2] += 1;
+    arrayOfNum[digitsNum - 1 - rand3] += 1;
+    console.log("I'm in hnndelpos 3");
+  }
+  return arrayOfNum;
+}
+export function handel(arrayOfNum, digitsNum) {
+  if (digitsNum == 4 || digitsNum == 5) {
+    handlePos(arrayOfNum, digitsNum, 0, 2);
+    console.log("I'm in hnndel 1");
+  } else if (digitsNum == 6 && digitsNum == 7) {
+    handlePos(arrayOfNum, digitsNum, 0, 2);
+    handlePos(arrayOfNum, digitsNum, 1, 3);
+    console.log("I'm in hnndel 2");
+  } else if (digitsNum == 8 || digitsNum == 9) {
+    handlePos(arrayOfNum, digitsNum, 0, 3);
+    handlePos(arrayOfNum, digitsNum, 1, 2);
+    handlePos(arrayOfNum, digitsNum, 2, 3);
+    console.log("I'm in hnndel 3");
+  }
+  return arrayOfNum;
+}
