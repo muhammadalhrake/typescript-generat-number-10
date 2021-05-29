b4fe878c8a8d656328f3709ce282eb01a7a76ebd
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-05-29 14:49:33 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -3,14 +3,22 @@ import './style.css';
 
 // imports
 import { generateQuestion } from './generateQuestion';
-
+import{generateQuestionR}from './generate-question'
 
 /* setInterval(()=>console.log(generateQuestion(5,4,"=10")),500); */
 
 /* setInterval(() =>testOfGeneratEqual(generatEqual(between(3,9))), 500); */
 /* setInterval(() => console.log(ansArray(111,+generatEqual(5).join(''))), 500); */
 
+function multQuestion(cont:number,ons:number,digits:number,stat:string){
+  let questions=new Array;
+  let a={answers:[],multiplyNum:0}
+  for(let i =0;i<cont;i++){
+    questions.push(generateQuestion(ons,digits,stat))
+  }
+  return questions
+}
 // Write TypeScript code!
-
+console.log(multQuestion(5,3,4,"=10"));
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
